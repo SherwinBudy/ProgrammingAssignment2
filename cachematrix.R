943b3b8eb7a5ad94872c34a75a4ff8b6e8c85c53
SherwinB <sherwinbudy88@yahoo.com>
2016-10-20 15:15:54 +0800

    Edit R File with solution to R Programming Assignment2, Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function makeCacheMatrix, creates a special "matrix".A list containing a function to
+## 1)   set the value of the matrix
+## 2)   get the value of the matrix
+## 3)   set the inverse value of the matrix
+## 4)   get the inverse value of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_mat <- NULL                         ## SETUP THE inv_mat TO NULL AS PLACE HOLDER FOR FUTURE USE
+        
+        set <- function(y) {
+                x <<- y 
+                inv_mat <<- NULL
+        }                                       ## SET MATRIX y, TO A NEW MATRIX y. AND RESET THE inv_mat TO NULL
+        
+        get <- function() x                     
+        set_inv <- function(solve) inv_mat <<- solve  
+        get_inv <- function() inv_mat  
+        list(set = set, get = get, set_inv = set_inv, get_inv = get_inv)
 }
 
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv_mat <- x$get_inv()
+        
+        if(!is.null(inv_mat)) {
+                print("getting cached inverse data")  ## FIRST CHECK IF MATRIX HAS BEEN INVERTED
+                return(inv_mat)
+                
+        }
+        data <- x$get()
+        inv_mat <- solve(data, ...)             ## COMPUTING THE INVERSE OF SQUARE MATRIX
+        x$set_inv(inv_mat)
+        inv_mat                                 ## RETURN A MATRIX THAT IS THE INVERSE OF 'x'
 }
+
